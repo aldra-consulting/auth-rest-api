7ed4fa06bb6cdfbf185214d9d8d6505e1b088204
Alexander Zakharov <alexander.zakharov@aldra.no>
2023-10-12 12:58:42 +0200

    feat: add pre and post middleware loggers

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import Provider from 'oidc-provider';
 
 import { ApplicationPhase } from '@project/enums';
 import env from '@project/env';
+import logger from '@project/utils/logging';
 
 import {
   compression,
@@ -26,6 +27,14 @@ export default async () =>
     provider.use(helmet);
     provider.use(referrer);
     provider.use(router(provider).routes());
+
+    provider.use(async (ctx, next) => {
+      logger.info('pre', { ctx });
+
+      await next();
+
+      logger.info('post', { ctx });
+    });
 
     provider.on(ApplicationPhase.SERVER_START, serverConfiguration);
     provider.on(ApplicationPhase.SERVER_START, queueListener);
