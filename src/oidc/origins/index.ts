50d1b6f4f79596694185bcda21fc940a0393b7f2
Alexander Zakharov <alexander.zakharov@aldra.no>
2024-01-26 14:12:08 +0100

    feat: add URLs for platform-web

--- src/oidc/origins/index.ts
+++ src/oidc/origins/index.ts
@@ -5,8 +5,14 @@ export default () =>
     development: [
       'http://localhost:8002',
       'http://localhost:8003',
+      'http://localhost:8004',
       'http://0.0.0.0:8002',
       'http://0.0.0.0:8003',
+      'http://0.0.0.0:8004',
     ],
-    production: ['https://www.aldra.no', 'https://www.id.aldra.no'],
+    production: [
+      'https://www.aldra.no',
+      'https://www.id.aldra.no',
+      'https://www.platform.aldra.no',
+    ],
   }) satisfies Record<EnvironmentVariables['NODE_ENV'], string[]>;
